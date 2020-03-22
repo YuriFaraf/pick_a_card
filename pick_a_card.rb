98a00d19b65f05f5041d150cc286737b8c833fdc
YuriFaraf <yura96@gmail.com>
2020-03-22 16:25:06 +0500

    symbols_instead_of_names

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,5 +1,4 @@
-# encoding: utf-8
 values = ['2', '3', '4', '5', '6', '7', '8', '9', 'J', 'Q', 'K', 'A']
-suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades']
+suits = %w(♦ ♠ ♥ ♣)
 
-puts "#{values.sample} of #{suits.sample}"
+puts "#{values.sample}#{suits.sample}"
